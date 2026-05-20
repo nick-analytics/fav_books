6daddfe33f84fa35f16b48d96a14077feb95b522
nick-analytics <ncallus@me.com>
2026-05-20 12:17:02 +1000

    user, customer and employee classes started

---
+++ js/customer.js
@@ -0,0 +1,11 @@
+//customer class inheritaes from user
+
+class Customer extends User{
+    constructor(id, role, firstName, lastName, email, password, phone, address, orderHistory){
+        super(id, role, firstName, lastName, email, password);
+        this.phone = phone;
+        this.address = address;
+        this.orderHistory = orderHistory;
+
+    }
+}
---
+++ js/user.js
@@ -0,0 +1,12 @@
+//Parent class. Holds user information. 
+
+class User{
+    constructor(id, role, firstName, lastName, email, password){
+        this.id = id;
+        this.role = role,
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+}
