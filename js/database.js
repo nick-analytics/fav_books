6fd6d9bec7e138ba260efe1f8e7aef4dac44fdcd
nick-analytics <ncallus@me.com>
2026-05-26 11:37:34 +1000

    db in progress

---
+++ js/database.js
@@ -0,0 +1,35 @@
+
+
+class Database{
+    constructor(){
+        this.initialise()
+    }
+
+// initlisation method to call json files 
+async initialiseJsonLocalStorage(){
+    const booksFilePath = '../json/books.json';
+    const usersFilePath = '../json/users.json';
+    const ordersFilePath = '../json/orders.json';
+
+    try {
+        if (!localStorage.getItem('books')){
+            const response = await  fetch(booksFilePath);
+            const data = await response.json();
+            localStorage.setItem('books', JSON.stringify(data));
+        }
+        if (!localStorage.getItem('users')){
+            const response = await  fetch(usersFilePath);
+            const data = await response.json();
+            localStorage.setItem('users', JSON.stringify(data));
+        }
+        if (!localStorage.getItem('orders')){
+            const response = await  fetch(ordersFilePath);
+            const data = await response.json();
+            localStorage.setItem('orders', JSON.stringify(data));
+        }
+    } catch (error){
+        console.error("Error loading JSON:", error)
+    }
+
+}
+}
