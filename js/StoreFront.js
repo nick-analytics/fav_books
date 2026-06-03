9df89e5d403fb39eff77a5288f7be9596fd2ce3d
nick-analytics <ncallus@me.com>
2026-06-03 13:26:17 +1000

    book laoding bug fix v2

--- js/StoreFront.js
+++ js/StoreFront.js
@@ -50,7 +50,7 @@ class StoreFront{
             this.setupDashboard();
         }
         if (document.querySelector('[data-genre="Fiction"]')){
-            await this.setupBooks();
+            this.setupBooks();
         }   
         if (document.querySelector('.book-detail-title')) {
             this.setupBookPage();
@@ -188,7 +188,7 @@ class StoreFront{
 
     //book methods. 
 
-    async setupBooks(){
+    setupBooks(){
         const books = this.database.getJsonFiles('books');
         const fictionBooks = books.filter(book => book.genre === 'Fiction');
         const fictionShelf = document.querySelector('[data-genre="Fiction"]');
